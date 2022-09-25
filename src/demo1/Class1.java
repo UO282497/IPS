2c74116a17d25920f9a150068ee02181d310e40f
jrubi <jrubi@DESKTOP-9MNN5QS>
2022-09-25 11:12:22 +0200

    Main añadido

--- src/demo1/Class1.java
+++ src/demo1/Class1.java
@@ -2,11 +2,8 @@ package demo1;
 
 public class Class1 {
 
-<<<<<<< HEAD
+
 	
-	public void main (String[] args) {
-		System.out.println("Test"); 
-	}
-=======
->>>>>>> branch 'master' of https://github.com/UO282497/IPS.git
+	
+
 }
